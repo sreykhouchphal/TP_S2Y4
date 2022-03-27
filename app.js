34312062edf42b53dd5c18d9db2b95b28818170c
phal sreykhouch <dimplekhouch29@gmail.com>
2022-03-27 22:44:12 +0700

    insert result

--- app.js
+++ app.js
@@ -10,12 +10,12 @@ var server = http.createServer(function(req, res){
     }else if(req.url === '/assets/book.png'){
         res.writeHead(200,{'Content-Type': 'image/png'});
         fs.createReadStream(__dirname + '/assets/book.png').pipe(res);
-    }else if (req.url === '/detail'){
+    }else if (req.url === './detail.html'){
         res.writeHead(200,{'Content-Type': 'text/html'});
         fs.createReadStream(__dirname + './detail.html').pipe(res);
     }
 });
 server.listen(3000, '127.0.0.1');
-console.log('Now, listening to port 3000');
+console.log('Now, listening to port 3000 http://localhost:3000/');
 
 
